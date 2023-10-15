59d7454938da5a82135f6b8c4fb3afa8f5f0ee15
brendon <36212954+brendonmiranda@users.noreply.github.com>
2023-10-15 20:24:00 +0100

    init commit

---
+++ crudcafe/src/main/java/com/brendon/crudcafe/CrudcafeApplication.java
@@ -0,0 +1,13 @@
+package com.brendon.crudcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudcafeApplication.class, args);
+	}
+
+}
---
+++ crudcafe/src/test/java/com/brendon/crudcafe/CrudcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brendon.crudcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
